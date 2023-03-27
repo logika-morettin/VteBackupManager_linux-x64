999bb33dfc3c70e18d644c1412d3713886e8af50
logika-morettin <129041091+logika-morettin@users.noreply.github.com>
2023-03-27 16:27:17 +0200

    add service autostart

--- VteBackupManager_Install.sh
+++ VteBackupManager_Install.sh
@@ -29,4 +29,5 @@ mv $PROJECT_NAME.service /etc/systemd/system
 chmod +x /etc/systemd/system/$PROJECT_NAME.service
 
 systemctl daemon-reload
+systemctl enable application.service
 systemctl start $PROJECT_NAME

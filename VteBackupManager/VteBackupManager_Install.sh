9b82acdabdc848c596f4b214584dd13d01a2e4e3
logika-morettin <129041091+logika-morettin@users.noreply.github.com>
2023-03-27 16:46:01 +0200

    fix install

--- VteBackupManager/VteBackupManager_Install.sh
+++ VteBackupManager/VteBackupManager_Install.sh
@@ -11,5 +11,5 @@ mv $PROJECT_NAME.service /etc/systemd/system
 chmod +x /etc/systemd/system/$PROJECT_NAME.service
 
 systemctl daemon-reload
-systemctl enable application.service
+systemctl enable $PROJECT_NAME.service
 systemctl start $PROJECT_NAME
